5677f8d151a6c8ddcc6ea8e7914a5d0ff30d35d3
suresh <b4u.sureshv@gmail.com>
2016-05-24 13:50:12 +0530

    first commit

---
+++ spark1/src/main/scala/org/test1/spark1/WordCount.scala
@@ -0,0 +1,30 @@
+package org.test1.spark1
+import org.apache.spark.SparkConf
+import org.apache.spark.SparkContext
+object WordCount {
+  
+  def main(args: Array[String]) = {
+
+    //Start the Spark context
+    val conf = new SparkConf()
+      .setAppName("WordCount")
+      .setMaster("local")
+    val sc = new SparkContext(conf)
+
+    //Read some example file to a test RDD
+    val test = sc.textFile("food.txt")
+
+    test.flatMap { line => //for each line
+      line.split(" ") //split the line in word by word. NB: we use flatMap because we return a list
+    }
+    .map { word => //for each word
+      (word,1) //return a key/value tuple, with the word as key and 1 as value
+    }
+    .reduceByKey(_ + _) //sum all of the value with same key
+    .saveAsTextFile("food.counts.txt") //save to a text file
+
+    //Stop the Spark context
+    sc.stop
+
+  }
+}
